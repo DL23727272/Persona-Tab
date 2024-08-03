61e6bb02f1089675440822426b08659fe96b0f8c
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-08-03 13:59:06 +0800

    updated delete contestant to delete also the scores due to fk issues

--- backend/deleteContestant.php
+++ backend/deleteContestant.php
@@ -5,12 +5,25 @@ include "../backend/myConnection.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $contestantID = $_POST['contestantID'];
 
-    $sql = "DELETE FROM contestants WHERE idContestant='$contestantID'";
-    
-    if (mysqli_query($con, $sql)) {
+    // Start a transaction
+    mysqli_begin_transaction($con);
+
+    try {
+        // First, delete related scores
+        $deleteScoresSQL = "DELETE FROM scores WHERE contestantID='$contestantID'";
+        mysqli_query($con, $deleteScoresSQL);
+
+        // Then, delete the contestant
+        $deleteContestantSQL = "DELETE FROM contestants WHERE idContestant='$contestantID'";
+        mysqli_query($con, $deleteContestantSQL);
+
+        // Commit the transaction
+        mysqli_commit($con);
         echo json_encode(["status" => "success", "message" => "Contestant deleted successfully"]);
-    } else {
-        echo json_encode(["status" => "error", "message" => "Error deleting contestant"]);
+    } catch (Exception $e) {
+        // Rollback the transaction on error
+        mysqli_rollback($con);
+        echo json_encode(["status" => "error", "message" => "Error deleting contestant: " . $e->getMessage()]);
     }
 }
 ?>
